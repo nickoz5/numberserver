6716c5ba89fd92eb208c06274157ed1199329340
allw <allanwiese@gmail.com>
2020-10-29 18:10:24 -0300

    Update BuildNumber.py
    Updated to Python 3.9
    Version on the request path
    Parameter for update

--- BuildNumber.py
+++ BuildNumber.py
@@ -1,5 +1,6 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import socket
+from urllib.parse import urlparse
 
 class NumberServer(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -10,23 +11,37 @@ class NumberServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
         
+        update = 'n'
+
+        query = urlparse(self.path).query
+        if query:
+            query_components = dict(qc.split("=") for qc in query.split("&"))
+            update = query_components["update"]
+
         # Try and open number file and read number
         try:
-            f = open("number.txt", "r")
+            index = self.path.index("?")-1 if self.path.find("?") != -1 else len(self.path)
+            fname = self.path[1:1+index] + "_releasenumber.txt"
+            f = open(fname, "r")
             number = int(f.read())
             f.close()
-        except IOError, ValueError:
-            # if we fail then just assume 1
-            number = 1
+        except (IOError, ValueError):
+            # if we fail then just assume 0
+            number = 0
 
         # send the number to the client
-        self.wfile.write(str(number))
+        result = number+1 if update == 'y' else number
+        self.wfile.write(str(result).encode(encoding='utf_8'))
         
-        # write incremented number back to file
-        f = open("number.txt", "w")
-        number = str(number+1)
-        f.write(number)
-        f.close()
+        if update == 'y':
+            # write incremented number back to file
+            try:
+                f = open(fname, "w")
+                numb = str(number+1)
+                f.write(numb)
+                f.close()
+            except (IOError, ValueError):
+                print ('Error')
 
     def do_HEAD(self):
         self._set_headers()
@@ -34,7 +49,7 @@ class NumberServer(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=NumberServer, port=80):
     server_address = ('0.0.0.0', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting number server...'
+    print ('Starting number server...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
